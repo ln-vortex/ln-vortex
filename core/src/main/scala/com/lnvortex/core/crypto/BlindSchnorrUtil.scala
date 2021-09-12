38d83599373e07b56988682157039e90c5206ff0
benthecarman <benthecarman@live.com>
2021-09-12 15:31:50 -0500

    Refactor

--- core/src/main/scala/com/lnvortex/core/crypto/BlindSchnorrUtil.scala
+++ core/src/main/scala/com/lnvortex/core/crypto/BlindSchnorrUtil.scala
@@ -3,8 +3,6 @@ package com.lnvortex.core.crypto
 import org.bitcoins.crypto._
 import scodec.bits._
 
-import scala.annotation.tailrec
-
 object BlindSchnorrUtil {
 
   private def computeBlindedHash(
@@ -81,66 +79,5 @@ object BlindSchnorrUtil {
       s"Blind signature ${blindSig.hex} is invalid for message ${message.toHex}.")
 
     sig
-  }
-}
-
-case class BlindingTweaks(
-    nonceTweak: FieldElement,
-    challengeTweak: FieldElement,
-    keyTweak: FieldElement)
-
-object BlindingTweaks {
-
-  private def isValidBIP340Tweak(
-      nonTweakedKey: ECPublicKey,
-      tweak: FieldElement): Option[FieldElement] = {
-    val tweakedKey = nonTweakedKey.add(tweak.getPublicKey)
-    if (tweakedKey == tweakedKey.schnorrPublicKey.publicKey) {
-      Some(tweak)
-    } else {
-      None
-    }
-  }
-
-  @tailrec
-  def freshBlindingTweaks(
-      signerPubKey: SchnorrPublicKey,
-      signerNonce: SchnorrNonce): BlindingTweaks = {
-    val nonceTweakRaw = ECPrivateKey.freshPrivateKey.fieldElement
-    val challengeTweak = ECPrivateKey.freshPrivateKey.fieldElement
-
-    val tweaksOpt = for {
-      nonceTweak <- isValidBIP340Tweak(signerPubKey.publicKey
-                                         .tweakMultiply(challengeTweak)
-                                         .add(signerNonce.publicKey),
-                                       nonceTweakRaw)
-    } yield BlindingTweaks(nonceTweak, challengeTweak, FieldElement.zero)
-
-    tweaksOpt match {
-      case Some(tweaks) => tweaks
-      case None         => freshBlindingTweaks(signerPubKey, signerNonce)
-    }
-  }
-
-  @tailrec
-  def freshBlindingTweaksWithKeyTweak(
-      signerPubKey: SchnorrPublicKey,
-      signerNonce: SchnorrNonce): BlindingTweaks = {
-    val nonceTweakRaw = ECPrivateKey.freshPrivateKey.fieldElement
-    val challengeTweak = ECPrivateKey.freshPrivateKey.fieldElement
-    val keyTweakRaw = ECPrivateKey.freshPrivateKey.fieldElement
-
-    val tweaksOpt = for {
-      keyTweak <- isValidBIP340Tweak(signerPubKey.publicKey, keyTweakRaw)
-      nonceTweak <- isValidBIP340Tweak(signerPubKey.publicKey
-                                         .tweakMultiply(challengeTweak)
-                                         .add(signerNonce.publicKey),
-                                       nonceTweakRaw)
-    } yield BlindingTweaks(nonceTweak, challengeTweak, keyTweak)
-
-    tweaksOpt match {
-      case Some(tweaks) => tweaks
-      case None         => freshBlindingTweaksWithKeyTweak(signerPubKey, signerNonce)
-    }
   }
 }
