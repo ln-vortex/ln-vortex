376fb8cd199734559d445d4daf582a295463cbe9
benthecarman <benthecarman@live.com>
2021-09-29 16:57:12 -0500

    Add cancel channel test

--- client-test/src/test/scala/com/lnvortex/client/lnd/LndChannelOpenerTest.scala
+++ client-test/src/test/scala/com/lnvortex/client/lnd/LndChannelOpenerTest.scala
@@ -2,7 +2,9 @@ package com.lnvortex.client.lnd
 
 import com.lnvortex.testkit.LndChannelOpenerFixture
 import org.bitcoins.core.currency.Satoshis
+import org.bitcoins.core.number.UInt32
 import org.bitcoins.core.protocol.script.P2WSHWitnessSPKV0
+import org.bitcoins.core.protocol.transaction.TransactionOutPoint
 import org.bitcoins.testkit.async.TestAsyncUtil
 import org.bitcoins.testkit.lnd.LndRpcTestUtil
 
@@ -71,5 +73,54 @@ class LndChannelOpenerTest extends LndChannelOpenerFixture {
         _ <- TestAsyncUtil.awaitConditionF(() =>
           lndB.listChannels().map(_.nonEmpty))
       } yield succeed
+  }
+
+  it must "cancel a channel" in { case (bitcoind, lndA, lndB) =>
+    val opener = LndChannelOpener(lndA)
+    val amount = Satoshis(100000)
+
+    for {
+      _ <- LndRpcTestUtil.connectLNNodes(lndA, lndB)
+      preChannelsA <- lndA.listChannels()
+      preChannelsB <- lndB.listChannels()
+      _ = assert(preChannelsA.isEmpty)
+      _ = assert(preChannelsB.isEmpty)
+
+      nodeId <- lndB.nodeId
+      fundDetails <- opener.initPSBTChannelOpen(nodeId = nodeId,
+                                                peerAddrOpt = None,
+                                                fundingAmount = amount,
+                                                privateChannel = false)
+
+      // construct psbt
+      psbt <- bitcoind
+        .walletCreateFundedPsbt(Vector.empty,
+                                Map(fundDetails.address -> fundDetails.amount))
+        .map(_.psbt)
+      // fund channel with psbt
+      _ <- opener.fundPendingChannel(fundDetails.id, psbt)
+
+      midChannelsA <- lndA.listChannels()
+      midChannelsB <- lndB.listChannels()
+      _ = assert(midChannelsA.isEmpty)
+      _ = assert(midChannelsB.isEmpty)
+
+      txid = psbt.transaction.txIdBE
+      vout = psbt.transaction.outputs.indexWhere(
+        _.scriptPubKey == fundDetails.address.scriptPubKey)
+      outPoint = TransactionOutPoint(txid, UInt32(vout))
+      _ <- opener.cancelChannel(outPoint)
+
+      res <- bitcoind.walletProcessPSBT(psbt)
+      tx <- Future.fromTry(res.psbt.extractTransactionAndValidate)
+      _ <- bitcoind.sendRawTransaction(tx)
+      _ <- bitcoind.getNewAddress.flatMap(bitcoind.generateToAddress(6, _))
+
+      // await for lnds to see channel
+      _ <- TestAsyncUtil.awaitConditionF(() =>
+        lndA.listChannels().map(_.isEmpty))
+      _ <- TestAsyncUtil.awaitConditionF(() =>
+        lndB.listChannels().map(_.isEmpty))
+    } yield succeed
   }
 }
