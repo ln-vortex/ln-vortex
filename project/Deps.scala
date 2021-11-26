8eba23fa7b42e62389fc9831090e0f50d79a3544
Scala Steward <me@scala-steward.org>
2021-11-26 02:08:23 +0100

    Update javafx-base, javafx-controls, ... to 18-ea+7

--- project/Deps.scala
+++ project/Deps.scala
@@ -25,7 +25,7 @@ object Deps {
     val akkaActorV: String = akkaStreamV
 
     val scalaFxV = "16.0.0-R25"
-    val javaFxV = "17-ea+8"
+    val javaFxV = "18-ea+7"
     val bitcoinsV = "1.8.0-47-019c9b26-SNAPSHOT"
 
     val grizzledSlf4jV = "1.3.4"
