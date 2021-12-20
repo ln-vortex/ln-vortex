290edc718dde7c88fb22954ba0e62e477f21a99c
Scala Steward <me@scala-steward.org>
2021-12-20 14:12:26 +0100

    Update akka-actor, akka-slf4j, akka-stream to 2.6.18

--- project/Deps.scala
+++ project/Deps.scala
@@ -21,7 +21,7 @@ object Deps {
 
   object V {
     val akkaV = "10.2.7"
-    val akkaStreamV = "2.6.17"
+    val akkaStreamV = "2.6.18"
     val akkaActorV: String = akkaStreamV
 
     val scalaFxV = "16.0.0-R25"
