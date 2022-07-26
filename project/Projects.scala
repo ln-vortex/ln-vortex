b92c48d4cce200fb40864c37aa15d1898f80db95
benthecarman <benthecarman@live.com>
2022-07-25 21:39:17 -0500

    Auto download needed binaries (#107)

--- project/Projects.scala
+++ project/Projects.scala
@@ -1,6 +1,9 @@
 import sbt._
 
 object Projects {
+  val root = project in file("..")
   val core = project in file("..") / "core"
   val clightning = project in file("..") / "clightning"
+  val lnd = project in file("..") / "lnd"
+  val bitcoind = project in file("..") / "bitcoind"
 }
