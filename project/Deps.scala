6e2982f0fa9e62fa63a186a5c725bb3283c9754c
benthecarman <benthecarman@live.com>
2022-09-14 16:22:01 -0500

    Update bitcoin-s

--- project/Deps.scala
+++ project/Deps.scala
@@ -7,7 +7,7 @@ object Deps {
     val akkaStreamV = "2.6.20"
     val akkaActorV: String = akkaStreamV
 
-    val bitcoinsV = "1.9.3-31-20ed1dca-SNAPSHOT"
+    val bitcoinsV = "1.9.4-1-6b479e87-SNAPSHOT"
 
     val scoptV = "4.1.0"
 
