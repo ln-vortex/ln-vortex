025aef12de2cf72d6855630a1cb4d4eacd5748bd
benthecarman <benthecarman@live.com>
2022-09-20 21:08:07 -0500

    Add end-to-end tests for CLN

---
+++ testkit/src/main/scala/com/lnvortex/testkit/CLNClientServerPairFixture.scala
@@ -0,0 +1,99 @@
+package com.lnvortex.testkit
+
+import com.lnvortex.client.VortexClient
+import com.lnvortex.clightning.CLightningVortexWallet
+import com.lnvortex.core.api.CoordinatorAddress
+import com.lnvortex.server.coordinator.VortexCoordinator
+import com.lnvortex.server.networking.VortexHttpServer
+import com.typesafe.config.ConfigFactory
+import org.bitcoins.core.config.RegTest
+import org.bitcoins.core.script.ScriptType
+import org.bitcoins.lnd.rpc.LndRpcClient
+import org.bitcoins.testkit.EmbeddedPg
+import org.bitcoins.testkit.async.TestAsyncUtil
+import org.bitcoins.testkit.fixtures.BitcoinSFixture
+import org.bitcoins.testkit.rpc.CachedBitcoindV23
+import org.scalatest.FutureOutcome
+
+import scala.reflect.io.Directory
+
+trait CLNClientServerPairFixture
+    extends BitcoinSFixture
+    with CachedBitcoindV23
+    with LnVortexTestUtils
+    with EmbeddedPg {
+
+  override type FixtureParam =
+    (VortexClient[CLightningVortexWallet], VortexCoordinator, LndRpcClient)
+
+  def isNetworkingTest: Boolean
+
+  def outputScriptType: ScriptType
+  def changeScriptType: ScriptType
+  def inputScriptType: ScriptType
+
+  override def withFixture(test: OneArgAsyncTest): FutureOutcome = {
+    makeDependentFixture[(VortexClient[CLightningVortexWallet],
+                          VortexCoordinator,
+                          LndRpcClient)](
+      () => {
+        val scriptTypeConfig =
+          ConfigFactory
+            .parseString(s"""
+                            |coordinator.outputScriptType = $outputScriptType
+                            |coordinator.changeScriptType = $changeScriptType
+                            |coordinator.inputScriptType = $inputScriptType
+                            |""".stripMargin)
+        implicit val (clientConfig, serverConf) =
+          getTestConfigs(Vector(scriptTypeConfig))
+
+        for {
+          _ <- serverConf.start()
+          bitcoind <- cachedBitcoindWithFundsF
+          coordinator <- VortexCoordinator.initialize(bitcoind)
+          server = new VortexHttpServer(coordinator)
+          _ <- server.start()
+          addr <- server.getBinding.map(_.localAddress)
+
+          _ = assert(serverConf.outputScriptType == outputScriptType)
+
+          _ <- clientConfig.start()
+          coordinatorAddr = CoordinatorAddress("test", RegTest, addr)
+
+          (lnd, peerLnd) <- CLNTestUtils.createNodePair(bitcoind,
+                                                        inputScriptType)
+          client = VortexClient(CLightningVortexWallet(lnd), coordinatorAddr)(
+            system,
+            clientConfig)
+          _ <- client.start()
+
+          // wait for it to receive round params
+          _ <- TestAsyncUtil.awaitCondition(() =>
+            client.getCurrentRoundDetails.order > 0)
+
+          // don't send message if not networking test
+          _ = if (!isNetworkingTest) {
+            coordinator.connectionHandlerMap.clear()
+            coordinator.roundSubscribers.clear()
+          }
+        } yield (client, coordinator, peerLnd)
+      },
+      { case (client, coordinator, peerLnd) =>
+        for {
+          _ <- peerLnd.stop()
+          _ <- client.vortexWallet.stop()
+          _ <- client.stop()
+          _ <- client.config.stop()
+
+          _ <- coordinator.config.dropAll().map(_ => coordinator.config.clean())
+          _ = coordinator.stop()
+          _ <- coordinator.config.stop()
+        } yield {
+          val directory = new Directory(coordinator.config.baseDatadir.toFile)
+          directory.deleteRecursively()
+          ()
+        }
+      }
+    )(test)
+  }
+}
---
+++ testkit/src/main/scala/com/lnvortex/testkit/CLNTestUtils.scala
@@ -0,0 +1,94 @@
+package com.lnvortex.testkit
+
+import akka.actor.ActorSystem
+import com.bitcoins.clightning.rpc.CLightningRpcClient
+import com.lnvortex.clightning.CLightningVortexWallet
+import com.lnvortex.lnd.LndVortexWallet
+import org.bitcoins.core.currency._
+import org.bitcoins.core.script.ScriptType
+import org.bitcoins.lnd.rpc.LndRpcClient
+import org.bitcoins.rpc.client.common.BitcoindRpcClient
+import org.bitcoins.testkit.async.TestAsyncUtil
+import org.bitcoins.testkit.clightning.CLightningRpcTestClient
+import org.bitcoins.testkit.lnd._
+import org.bitcoins.testkit.util.FileUtil
+
+import scala.concurrent._
+import scala.concurrent.duration.DurationInt
+
+trait CLNTestUtils {
+
+  def fundNodes(
+      bitcoind: BitcoindRpcClient,
+      cln: CLightningRpcClient,
+      lnd: LndRpcClient,
+      scriptType: ScriptType)(implicit ec: ExecutionContext): Future[Unit] = {
+    val clnAddressType =
+      CLightningVortexWallet.addressTypeFromScriptType(scriptType)
+    val lndAddressType = LndVortexWallet.addressTypeFromScriptType(scriptType)
+
+    for {
+      addrA <- cln.getNewAddress(clnAddressType)
+      addrB <- cln.getNewAddress(clnAddressType)
+      addrC <- cln.getNewAddress(clnAddressType)
+      addrD <- lnd.getNewAddress(lndAddressType)
+      addrE <- lnd.getNewAddress(lndAddressType)
+      addrF <- lnd.getNewAddress(lndAddressType)
+
+      _ <- bitcoind.sendMany(
+        Map(addrA -> Bitcoins(1), addrB -> Bitcoins(2), addrC -> Bitcoins(3)))
+      _ <- bitcoind.sendMany(
+        Map(addrD -> Bitcoins(1), addrE -> Bitcoins(2), addrF -> Bitcoins(3)))
+      _ <- bitcoind.getNewAddress.flatMap(bitcoind.generateToAddress(6, _))
+    } yield ()
+  }
+
+  def createNodePair(
+      bitcoind: BitcoindRpcClient,
+      inputType: ScriptType)(implicit
+      ec: ExecutionContext): Future[(CLightningRpcClient, LndRpcClient)] = {
+    val actorSystemA =
+      ActorSystem.create("ln-vortex-cln-test-" + FileUtil.randomDirName)
+    val clnClient =
+      CLightningRpcTestClient.fromSbtDownload(Some(bitcoind), None)(
+        actorSystemA)
+
+    val actorSystemB =
+      ActorSystem.create("ln-vortex-lnd-test-" + FileUtil.randomDirName)
+    val lndClient =
+      LndRpcTestClient.fromSbtDownload(Some(bitcoind), LndTestUtils.lndVersion)(
+        actorSystemB)
+
+    val clientsF = for {
+      a <- clnClient.start()
+      b <- lndClient.start()
+    } yield (a, b)
+
+    def isSynced: Future[Boolean] = for {
+      (cln, lnd) <- clientsF
+      height <- bitcoind.getBlockCount
+
+      infoA <- cln.getInfo
+      infoB <- lnd.getInfo
+    } yield infoA.blockheight == height && infoB.syncedToChain
+
+    def isFunded: Future[Boolean] = for {
+      (cln, lnd) <- clientsF
+
+      balA <- cln.walletBalance()
+      balB <- lnd.walletBalance()
+    } yield balA.confirmedBalance == Bitcoins(6) &&
+      balB.confirmedBalance == Bitcoins(6)
+
+    for {
+      (cln, lnd) <- clientsF
+
+      _ <- fundNodes(bitcoind, cln, lnd, inputType)
+
+      _ <- TestAsyncUtil.awaitConditionF(() => isSynced, interval = 1.second)
+      _ <- TestAsyncUtil.awaitConditionF(() => isFunded)
+    } yield (cln, lnd)
+  }
+}
+
+object CLNTestUtils extends CLNTestUtils
