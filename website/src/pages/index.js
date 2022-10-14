12506c8ec9e995f236ce5d074ffedeb6ce6320f8
Suhail Saqan <43693074+suhailsaqan@users.noreply.github.com>
2022-10-13 20:06:29 -0700

    add vortex gui to welcome page (#367)

--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -135,6 +135,26 @@ function Home() {
                         </div>
                     </section>
                 )}
+                <section className={classnames(styles.features)}>
+                    <div className="container">
+                        <div className="row">
+                            <div style={{marginTop: '100px'}} className={classnames('col col--6 text--center', styles.feature)}>
+                                <h2>Easy to use</h2>
+                                <p>
+                                    Vortex comes with an easy to use GUI that is usable as a desktop application or as web server.
+                                    Vortex handles all the hard work and privacy is only a click away.
+                                </p>
+                            </div>
+                            <div className="col col--6" style={{
+                                display: 'flex',
+                                alignItems: 'center',
+                                justifyContent: 'center',
+                            }}>
+                                <img style={{borderRadius: 0}} src="img/home-screen.png" width={'500px'}/>
+                            </div>
+                        </div>
+                    </div>
+                </section>
             </main>
         </Layout>
     );
