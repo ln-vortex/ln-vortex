916f2d4b556e390d33a65e395a56222726b258c2
benthecarman <benthecarman@live.com>
2022-10-18 23:11:37 -0500

    Update metadata image to png

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -54,7 +54,7 @@ const config = {
     themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
         {
-            image: 'img/logo.svg',
+            image: 'img/vortex_logo1000.png',
             colorMode: {
                 defaultMode: 'dark',
                 disableSwitch: true,
